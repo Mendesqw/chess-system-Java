fee29c6852311486bded29c60b120848e4989fc6
Mateus <mateus.mendes0612@gmail.com>
2026-03-30 16:35:41 -0300

    Possible Moves

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -15,6 +15,12 @@ public class Queen extends ChessPiece {
         return "Q";
     }
 
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean mat[][] = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
+    }
+
     
 
 }
