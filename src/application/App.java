9d3a65882415e0655f69c8ea46b5c086127d60df
Mateus <mateus.mendes0612@gmail.com>
2026-04-22 17:35:11 -0300

    Turn Change implementatio

--- src/application/App.java
+++ src/application/App.java
@@ -16,7 +16,7 @@ public class App {
         while (true) {
             try {
                 UI.clearScreen();
-                UI.printBoard(chessmatch.getPieces());
+                UI.printMatch(chessmatch);
                 System.out.println();
 
                 System.out.print("Source: ");
